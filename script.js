92a7169c6e02ff9ca35fb8b30b50f82a5faf1cd9
Dharmendra Pandit <dharmendra763297@gmail.com>
2023-09-26 14:42:37 +0530

    tweet

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const jsonData = {};
+const quotes = document.getElementById("quotes")
+const author = document.getElementById("author")
 const getQuote = async () => {
     const api = "https://api.quotable.io/random";
     try {
@@ -7,10 +8,19 @@ const getQuote = async () => {
         // console.log(quotesData);
         // console.log(quotesData.content);
         // console.log(quotesData.authorSlug);
-        document.getElementById("quotes").innerText = quotesData.content;
-        document.getElementById("author").innerHTML = `Author- ${quotesData.authorSlug}`;
+        // document.getElementById("quotes").innerText = quotesData.content;
+        quotes.innerText = quotesData.content;
+        // document.getElementById("author").innerHTML = `Author- ${quotesData.authorSlug}`;
+        author.innerHTML = `Author- ${quotesData.authorSlug}`;
     } catch (error) {
 
     }
+}
+
+const tweet = () => {
+    const quotesText = quotes.innerText;
+    const authorText = author.innerText;
+    const url = `https://twitter.com/intent/tweet?text=${encodeURIComponent(quotesText)}%0A${encodeURIComponent(authorText)}`;
+    window.open(url);
 }
 // getQuote()
